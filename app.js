70303dee7438b246f8ef230cc58b3cefa476bfc5
Prakash Narayanan <myprak@gmail.com>
2017-06-20 13:59:33 +0700

    test

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var server = http.createServer(function(req, res) {
 	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.end("hellodkkddd world!\n");
+	res.end("hellccodkkddd world!\n");
     });
 server.listen(3000);
